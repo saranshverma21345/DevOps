1ebe7f7879091dd27694a9634fa85ecbf7008cb5
saranshverma21345 <82048395+saranshverma21345@users.noreply.github.com>
2024-07-16 00:16:30 +0530

    Update DevOps_Testing.py

--- scripts/DevOps_Testing.py
+++ scripts/DevOps_Testing.py
@@ -1,7 +1,7 @@
 import pandas as py
 
 py = file_name
-wasd - py.read_html('https://en.wikipedia.org/wiki/The_Sopranos')
+wasd = py.read_html('https://en.wikipedia.org/wiki/The_Sopranos')
 
 print(len(wasd))
 
